b79ff2e8e54711a59e8ff436dc30a35539dceade
matheuslima99 <maths.soares99@gmail.com>
2022-12-06 10:16:58 -0300

    Visual Structure - Creating carousel

--- src/styles/pages/home.ts
+++ src/styles/pages/home.ts
@@ -7,13 +7,11 @@ export const HomeContainer = styled("main", {
   marginLeft: "auto",
 
   display: "flex",
-  gap: "3rem",
 });
 
 export const Product = styled("a", {
   background: "linear-gradient(100deg, #1ea483 0%, #7465d4 100%)",
   borderRadius: 8,
-  padding: "0.25rem",
   cursor: "pointer",
   position: "relative",
 
