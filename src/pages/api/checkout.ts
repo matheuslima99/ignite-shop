8088f73952fab84350048a7676923fe147a172ea
matheuslima99 <maths.soares99@gmail.com>
2022-12-08 15:41:58 -0300

    Product & Checkout - Purchase data on success

--- src/pages/api/checkout.ts
+++ src/pages/api/checkout.ts
@@ -16,7 +16,7 @@ export default async function handler(
     return res.status(404).json({ error: "Price not found." });
   }
 
-  const sucessUrl = `${process.env.NEXT_URL}/success`;
+  const sucessUrl = `${process.env.NEXT_URL}/success?session_id={CHECKOUT_SESSION_ID}`;
   const cancelUrl = `${process.env.NEXT_URL}/`;
 
   const checkoutSession = await stripe.checkout.sessions.create({
